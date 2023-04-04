c14d4b326622c8c37a7f2f5b1df4e9710fd6a410
Dogan Ulus <doganulus@gmail.com>
2023-04-04 10:07:44 +0000

    Add tests for Boost JSON extensions

--- tests/boost_json_ext.test.cpp
+++ tests/boost_json_ext.test.cpp
@@ -1,12 +1,11 @@
 #include "rvstd/boost_json_ext.hpp"
 
 #include <boost/json/value.hpp>
-#include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("Boost JSON Merge Patch", "core")  // NOLINT
 {
-  SECTION("Merge Patch: Object")
+  SECTION("Merge objects")
   {
     auto j1 = boost::json::value{{"a", 1}, {"b", 2}, {"c", 3}};
     auto j2 = boost::json::value{{"b", 4}, {"d", 5}};
@@ -17,43 +16,75 @@ TEST_CASE("Boost JSON Merge Patch", "core")  // NOLINT
     REQUIRE(j1 == j3);
   }
 
-  SECTION("RFC 7386 Example")
+  SECTION("Overwrite non-object with object")
   {
-    auto doc = boost::json::value{{"title", "Goodbye!"}};
-    auto patch = boost::json::value{{"title", "Hello!"}};
-    auto patched_doc = boost::json::value{{"title", "Hello!"}};
+    auto target = boost::json::value{1};
+    auto patch = boost::json::value{{"a", 1}};
+    auto expected = boost::json::value{{"a", 1}};
 
-    boost::json::merge_patch(doc, patch);
+    boost::json::merge_patch(target, patch);
 
-    REQUIRE(doc == patched_doc);
+    REQUIRE(target == expected);
+  }
+
+  SECTION("Overwrite object with non-object")
+  {
+    auto target = boost::json::value{{"a", 1}};
+    auto patch = boost::json::value{1};
+    auto expected = boost::json::value{1};
+
+    boost::json::merge_patch(target, patch);
+
+    REQUIRE(target == expected);
+  }
+
+  SECTION("Overwrite array with array")
+  {
+    auto target = boost::json::value{1, 2, 3};
+    auto patch = boost::json::value{4, 5, 6};
+    auto expected = boost::json::value{4, 5, 6};
+
+    boost::json::merge_patch(target, patch);
+
+    REQUIRE(target == expected);
+  }
+
+  SECTION("Erase scalar with null")
+  {
+    auto target = boost::json::value{1};
+    auto patch = boost::json::value{nullptr};
+    auto expected = boost::json::value{nullptr};
+
+    boost::json::merge_patch(target, patch);
+
+    REQUIRE(target == expected);
+  }
+
+  SECTION("Erase array with null")
+  {
+    auto target = boost::json::value{1, 2, 3};
+    auto patch = boost::json::value{nullptr};
+    auto expected = boost::json::value{nullptr};
+
+    boost::json::merge_patch(target, patch);
+
+    REQUIRE(target == expected);
+  }
+
+  SECTION("Erase object with null")
+  {
+    auto target = boost::json::value{{"a", 1}, {"b", 2}};
+    auto patch = boost::json::value{nullptr};
+    auto expected = boost::json::value{nullptr};
+
+    boost::json::merge_patch(target, patch);
+
+    REQUIRE(target == expected);
   }
 
   SECTION("RFC 7386 Example")
   {
-    auto doc = boost::json::value{{"title", "Goodbye!"}};
-    auto patch = boost::json::value{{"title", nullptr}};
-    auto patched_doc = boost::json::object{};
-
-    boost::json::merge_patch(doc, patch);
-
-    REQUIRE(doc == patched_doc);
-  }
-
-  SECTION("RFC 7386 Example")
-  {
-    auto doc = boost::json::value{
-      {"author", {{"givenName", "John"}, {"familyName", "Doe"}}}};
-    auto patch = boost::json::value{{"author", {{"familyName", nullptr}}}};
-    auto patched_doc = boost::json::value{{"author", {{"givenName", "John"}}}};
-
-    boost::json::merge_patch(doc, patch);
-
-    REQUIRE(doc == patched_doc);
-  }
-
-  SECTION("RFC 7386 Example")
-  {
-    auto doc = boost::json::value{
+    auto target = boost::json::value{
       {"title", "Goodbye!"},
       {"author", {{"givenName", "John"}, {"familyName", "Doe"}}},
       {"tags", {"example", "sample"}},
@@ -63,15 +94,15 @@ TEST_CASE("Boost JSON Merge Patch", "core")  // NOLINT
       {"author", {{"familyName", nullptr}}},
       {"phoneNumber", "+01-123-456-7890"},
       {"tags", {"example"}}};
-    auto patched_doc = boost::json::value{
+    auto expected = boost::json::value{
       {"title", "Hello!"},
       {"author", {{"givenName", "John"}}},
       {"tags", {"example"}},
       {"phoneNumber", "+01-123-456-7890"},
       {"content", "This will be unchanged"}};
 
-    boost::json::merge_patch(doc, patch);
+    boost::json::merge_patch(target, patch);
 
-    REQUIRE(doc == patched_doc);
+    REQUIRE(target == expected);
   }
 }
