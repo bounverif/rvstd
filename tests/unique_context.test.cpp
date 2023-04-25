2ed5142303513dee278673120620e19b0ff5bf5d
Dogan Ulus <dogan.ulus@boun.edu.tr>
2023-04-25 19:47:44 +0000

    Return identifier from context

--- tests/unique_context.test.cpp
+++ tests/unique_context.test.cpp
@@ -9,23 +9,25 @@
 TEST_CASE("Unique Context", "core")  // NOLINT
 {
   using namespace rvstd;
+  auto ctx = rvstd::unique_context();
+
+  auto idx = rvstd::unique_identifier("key");
+  auto type = rvstd::unique_identifier("type");
+  auto rel1 = rvstd::unique_identifier("rel1");
+  auto arg1 = rvstd::unique_identifier("arg1");
+  auto arg2 = rvstd::unique_identifier("arg2");
 
   SECTION("Default Construction")
   {
-    auto ctx = rvstd::unique_context();
     REQUIRE(ctx.empty());
   }
 
   SECTION("Emplace")
   {
-    auto ctx = rvstd::unique_context();
-    auto idx = rvstd::unique_identifier("key");
-    auto type = rvstd::unique_identifier("type");
-    auto rel1 = rvstd::unique_identifier("rel1");
-    auto arg1 = rvstd::unique_identifier("arg1");
-    auto arg2 = rvstd::unique_identifier("arg2");
-
     ctx.emplace(idx, resource(type, {{rel1, {arg1, arg2}}}));
-    REQUIRE(ctx.empty());
+
+    REQUIRE(ctx.size() == 1);
   }
+
+  SECTION("Add") {}
 }
