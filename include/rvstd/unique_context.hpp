bb0ae676675d9b6d8595ba29d4acd139db41a04b
Dogan Ulus <dogan.ulus@boun.edu.tr>
2023-04-29 20:18:13 +0000

    Standardize `unique_context` interface

--- include/rvstd/unique_context.hpp
+++ include/rvstd/unique_context.hpp
@@ -1,6 +1,7 @@
 #ifndef RVSTD_CONTEXT_HPP
 #define RVSTD_CONTEXT_HPP
 
+#include "rvstd/config.hpp"
 #include "rvstd/resource.hpp"
 #include "rvstd/types.hpp"
 #include "rvstd/unique_identifier.hpp"
@@ -9,67 +10,75 @@
 
 namespace rvstd {
 
-/**
- * This is a container adapter
- */
 struct unique_context {
-  using resource_type = unique_identifier;
-  using resource_identifier = unique_identifier;
+  using key_type = unique_identifier;
+  using mapped_type = resource;
+  using value_type = std::pair<const key_type, mapped_type>;
   using size_type = std::size_t;
+  using difference_type = std::ptrdiff_t;
+  using key_compare = std::less<key_type>;
 
-  using container_type =
-    boost::container::flat_map<resource_identifier, resource>;
-
+  using container_type = std::map<key_type, mapped_type>;
+  using reference = container_type::reference;
+  using const_reference = container_type::const_reference;
   using iterator = container_type::const_iterator;
   using const_iterator = container_type::const_iterator;
 
   using type = unique_context;
 
   unique_context();
-  // explicit unique_context(options opts = options());
-  // explicit unique_context(string_view name, options opts = options());
 
   // iterators
   RVSTD_NODISCARD
+  auto begin() noexcept -> iterator;
+  RVSTD_NODISCARD
+  auto end() noexcept -> iterator;
+  RVSTD_NODISCARD
   auto begin() const noexcept -> const_iterator;
-
   RVSTD_NODISCARD
   auto end() const noexcept -> const_iterator;
-
   RVSTD_NODISCARD
   auto cbegin() const noexcept -> const_iterator;
-
   RVSTD_NODISCARD
   auto cend() const noexcept -> const_iterator;
 
   // capacity
   RVSTD_NODISCARD
   bool empty() const noexcept;
-
   RVSTD_NODISCARD
   auto size() const noexcept -> size_type;
+  RVSTD_NODISCARD
+  auto max_size() const noexcept -> size_type;
 
   // modifiers
-  auto emplace(resource const& src) -> resource_identifier;
-  auto emplace(resource&& src) -> resource_identifier;
-  auto emplace(resource_identifier key, resource const& src)
-    -> resource_identifier;
-  auto emplace(resource_identifier key, resource&& src) -> resource_identifier;
-  auto emplace(string_view name, resource const& src) -> resource_identifier;
-  auto emplace(string_view name, resource&& src) -> resource_identifier;
+  void clear() noexcept;
+  auto emplace(resource const& src) -> unique_identifier;
+  auto emplace(resource&& src) -> unique_identifier;
+  auto emplace(unique_identifier key, resource const& src) -> unique_identifier;
+  auto emplace(unique_identifier key, resource&& src) -> unique_identifier;
+  auto emplace(string_view name, resource const& src) -> unique_identifier;
+  auto emplace(string_view name, resource&& src) -> unique_identifier;
+  auto erase(const_iterator pos) -> iterator;
+  auto erase(const_iterator first, const_iterator last) -> iterator;
+  bool erase(unique_identifier key);
+  bool erase(string_view key);
 
   // lookup
+  RVSTD_NODISCARD auto find(unique_identifier key) const -> const_iterator;
   RVSTD_NODISCARD
-  auto find(resource_identifier key) const -> const_iterator;
-
+  auto lookup(string_view key) const -> unique_identifier;
   RVSTD_NODISCARD
-  auto lookup(string_view key) const -> resource_identifier;
+  auto lookup(resource const& src) const -> unique_identifier;
   RVSTD_NODISCARD
-  auto lookup(resource const& src) const -> resource_identifier;
+  bool contains(unique_identifier key) const;
+  RVSTD_NODISCARD
+  bool contains(string_view key) const;
+  RVSTD_NODISCARD
+  bool contains(resource const& src) const;
 
  private:
-  boost::container::flat_map<size_type, resource_identifier> hashes_;
-  boost::container::flat_map<resource_identifier, resource> table_;
+  std::map<size_type, unique_identifier> hashes_;
+  std::map<unique_identifier, resource> table_;
 };
 
 }  // namespace rvstd
--- src/rvstd/unique_context.cpp
+++ src/rvstd/unique_context.cpp
@@ -7,51 +7,66 @@ namespace rvstd {
 
 unique_context::unique_context() = default;
 
+auto unique_context::begin() noexcept -> iterator
+{
+  return table_.begin();
+}
+auto unique_context::end() noexcept -> iterator
+{
+  return table_.end();
+}
 auto unique_context::begin() const noexcept -> const_iterator
 {
-  return this->cbegin();
+  return table_.begin();
 }
-
+auto unique_context::end() const noexcept -> const_iterator
+{
+  return table_.end();
+}
 auto unique_context::cbegin() const noexcept -> const_iterator
 {
   return table_.cbegin();
 }
-
-auto unique_context::end() const noexcept -> const_iterator
-{
-  return this->cend();
-}
-
 auto unique_context::cend() const noexcept -> const_iterator
 {
   return table_.cend();
 }
 
-auto unique_context::size() const noexcept -> size_type
-{
-  return table_.size();
-}
-
+// capacity
 bool unique_context::empty() const noexcept
 {
   return table_.empty();
 }
+auto unique_context::size() const noexcept -> size_type
+{
+  return table_.size();
+}
+auto unique_context::max_size() const noexcept -> size_type
+{
+  return table_.max_size();
+}
 
-auto unique_context::find(resource_identifier key) const -> const_iterator
+// modifiers
+void unique_context::clear() noexcept
+{
+  return table_.clear();
+}
+
+// lookup
+auto unique_context::find(unique_identifier key) const -> const_iterator
 {
   return table_.find(key);
 }
 
-auto unique_context::lookup(string_view key) const -> resource_identifier
+auto unique_context::lookup(string_view key) const -> unique_identifier
 {
-  if(auto const it = table_.find(resource_identifier(key));
-     it != table_.end()) {
+  if(auto const it = table_.find(unique_identifier(key)); it != table_.end()) {
     return it->first;
   }
   return nil_identifier();
 }
 
-auto unique_context::lookup(resource const& src) const -> resource_identifier
+auto unique_context::lookup(resource const& src) const -> unique_identifier
 {
   const auto h = std::hash<resource>{}(src);
   if(auto const it = hashes_.find(h); it != hashes_.end()) {
@@ -60,18 +75,18 @@ auto unique_context::lookup(resource const& src) const -> resource_identifier
   return nil_identifier();
 }
 
-auto unique_context::emplace(resource const& src) -> resource_identifier
+auto unique_context::emplace(resource const& src) -> unique_identifier
 {
-  return emplace(resource_identifier(), src);
+  return emplace(unique_identifier(), src);
 }
 
-auto unique_context::emplace(resource&& src) -> resource_identifier
+auto unique_context::emplace(resource&& src) -> unique_identifier
 {
-  return emplace(resource_identifier(), std::move(src));
+  return emplace(unique_identifier(), std::move(src));
 }
 
-auto unique_context::emplace(resource_identifier key, const resource& src)
-  -> resource_identifier
+auto unique_context::emplace(unique_identifier key, const resource& src)
+  -> unique_identifier
 {
   if(key == nil_identifier()) {
     return nil_identifier();
@@ -92,8 +107,8 @@ auto unique_context::emplace(resource_identifier key, const resource& src)
   return nil_identifier();
 }
 
-auto unique_context::emplace(resource_identifier key, resource&& src)
-  -> resource_identifier
+auto unique_context::emplace(unique_identifier key, resource&& src)
+  -> unique_identifier
 {
   if(key == nil_identifier()) {
     return nil_identifier();
@@ -114,15 +129,63 @@ auto unique_context::emplace(resource_identifier key, resource&& src)
 }
 
 auto unique_context::emplace(string_view name, resource const& src)
-  -> resource_identifier
+  -> unique_identifier
 {
-  return emplace(resource_identifier(name), src);
+  return emplace(unique_identifier(name), src);
 }
 
 auto unique_context::emplace(string_view name, resource&& src)
-  -> resource_identifier
+  -> unique_identifier
 {
-  return emplace(resource_identifier(name), std::move(src));
+  return emplace(unique_identifier(name), std::move(src));
+}
+
+auto unique_context::erase(const_iterator pos) -> iterator
+{
+  const auto h = std::hash<resource>{}(pos->second);
+  hashes_.erase(h);
+  return table_.erase(pos);
+}
+
+auto unique_context::erase(const_iterator first, const_iterator last)
+  -> iterator
+{
+  for(auto it = first; it != last; ++it) {
+    const auto h = std::hash<resource>{}(it->second);
+    hashes_.erase(h);
+  }
+  return table_.erase(first, last);
+}
+
+bool unique_context::erase(unique_identifier key)
+{
+  if(const auto it = table_.find(key); it != table_.end()) {
+    const auto h = std::hash<resource>{}(it->second);
+    hashes_.erase(h);
+    table_.erase(it);
+    return true;
+  }
+  return false;
+}
+
+bool unique_context::erase(string_view key)
+{
+  return this->erase(unique_identifier(key));
+}
+
+bool unique_context::contains(unique_identifier key) const
+{
+  return table_.find(key) != table_.end();
+}
+
+bool unique_context::contains(string_view key) const
+{
+  return table_.find(unique_identifier(key)) != table_.end();
+}
+
+bool unique_context::contains(resource const& src) const
+{
+  return lookup(src) != nil_identifier();
 }
 
 }  // namespace rvstd
